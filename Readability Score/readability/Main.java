11e72e9a2c680d0b02e824172fc6c8e11f958cb2
Alex Golub <55350522+Alex-Golub@users.noreply.github.com>
2020-06-21 20:50:50 +0300

    (Readability Score) Stage 2/4: Words and sentences. closes #10

--- Readability Score/readability/Main.java
+++ Readability Score/readability/Main.java
@@ -6,7 +6,15 @@ public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    int symbols = scanner.nextLine().length();
-    System.out.println(symbols > 100 ? "HARD" : "EASY");
+    // Split text by following punctuations: ! . ?
+    String[] sentences = scanner.nextLine().split("[!.?]");
+
+    // Sum total of words in each sentence
+    int totalWords = String.join(" ", sentences)
+            .split("\\s+")
+            .length;
+
+    System.out.println(totalWords * 1.0 / sentences.length > 10.0 ? "HARD" : "EASY");
+
   }
 }
